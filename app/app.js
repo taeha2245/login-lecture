61651421578ec7c9230957f11ca8ecbffb25d2e6
윤태하 <taeha2245@gmail.com>
2024-09-19 20:20:51 +0900

    프론트 javascript 파일 생성

--- app/app.js
+++ app/app.js
@@ -41,6 +41,8 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+// ${__dirname} -> app.js 의 위치 안에 있는 src 안에 있는 public을 정적경로로 설정
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);  // use -> 미들 웨어를 등록해주는 메서드.
 
