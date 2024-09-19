2d04cc0209461815270274a4ae9e837223395f6f
윤태하 <taeha2245@gmail.com>
2024-09-19 20:33:45 +0900

    DOM 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,14 @@
 "use strict";
 
-console.log("heelo");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pw : pw.value
+    };
+}
